5718fb6a9567f284ef2cbccc64853644cbeb8080
Jonas Metzener <jonas.metzener@adfinis-sygroup.ch>
2017-01-05 11:14:43 +0100

    Use sphinx RTD theme

--- docs/conf.py
+++ docs/conf.py
@@ -89,8 +89,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'adsy'
-html_theme_path = ['../adsy-sphinx-template.src/html']
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
