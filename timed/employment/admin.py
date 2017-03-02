3adb8db68edc8fd3e43385ae5c89193d737b0bc3
Jonas Metzener <jonas.metzener@adfinis-sygroup.ch>
2017-03-02 10:18:41 +0100

    * Fixed some docstrings * Use a custom admin form for admin validation * Test custom admin form

--- timed/employment/admin.py
+++ timed/employment/admin.py
@@ -1,8 +1,64 @@
 """Views for the admin interface."""
 
+import datetime
+
+from django import forms
 from django.contrib import admin
+from django.core.exceptions import ValidationError
 
 from timed.employment import models
+
+
+class EmploymentForm(forms.ModelForm):
+    """Custom form for the employment admin."""
+
+    def clean(self):
+        """Validate the employment as a whole.
+
+        Ensure the end date is after the start date and there is only one
+        active employment per user and there are no overlapping employments.
+
+        :throws: django.core.exceptions.ValidationError
+        :return: The cleaned data
+        :rtype:  dict
+        """
+        data = super().clean()
+
+        employments = models.Employment.objects.filter(user=data.get('user'))
+
+        if (
+            data.get('end_date') and
+            data.get('start_date') >= data.get('end_date')
+        ):
+            raise ValidationError('The end date must be after the start date')
+
+        if (
+            employments.filter(end_date__isnull=True) and
+            data.get('end_date') is None
+        ):
+            raise ValidationError('A user can only have one active employment')
+
+        if any([
+            e.start_date <= (
+                data.get('end_date') if
+                data.get('end_date') else
+                datetime.date.today()
+            ) and data.get('start_date') <= (
+                e.end_date if e.end_date else datetime.date.today()
+            )
+            for e
+            in employments
+        ]):
+            raise ValidationError('A user can\'t have multiple employments '
+                                  'at the same time')
+
+        return data
+
+    class Meta:
+        """Meta information for the employment form."""
+
+        fields = '__all__'
+        model = models.Employment
 
 
 @admin.register(models.Location)
@@ -17,6 +73,7 @@ class LocationAdmin(admin.ModelAdmin):
 class EmploymentAdmin(admin.ModelAdmin):
     """Employment admin view."""
 
+    form         = EmploymentForm
     list_display = ['__str__', 'percentage', 'location']
     list_filter  = ['location', 'user']
 
--- timed/employment/tests/test_employment.py
+++ timed/employment/tests/test_employment.py
@@ -3,11 +3,11 @@
 import datetime
 
 import pytest
-from django.core.exceptions import ValidationError
 from django.core.urlresolvers import reverse
 from rest_framework.status import (HTTP_200_OK, HTTP_401_UNAUTHORIZED,
                                    HTTP_405_METHOD_NOT_ALLOWED)
 
+from timed.employment.admin import EmploymentForm
 from timed.employment.factories import EmploymentFactory
 from timed.jsonapi_test_case import JSONAPITestCase
 
@@ -100,16 +100,19 @@ class EmploymentTests(JSONAPITestCase):
 
     def test_employment_unique_active(self):
         """Should only be able to have one active employment per user."""
-        e = EmploymentFactory.build(user=self.user, end_date=None)
+        form = EmploymentForm({
+            'end_date': None
+        }, instance=self.employments[1])
 
-        with pytest.raises(ValidationError):
-            e.clean()
+        with pytest.raises(ValueError):
+            form.save()
 
     def test_employment_unique_range(self):
         """Should only be able to have one employment at a time per user."""
-        e = EmploymentFactory.build(user=self.user,
-                                    start_date=datetime.date(2009, 1, 1),
-                                    end_date=datetime.date(2016, 1, 1))
+        form = EmploymentForm({
+            'start_date': datetime.date(2009, 1, 1),
+            'end_date':   datetime.date(2016, 1, 1)
+        }, instance=self.employments[0])
 
-        with pytest.raises(ValidationError):
-            e.clean()
+        with pytest.raises(ValueError):
+            form.save()
