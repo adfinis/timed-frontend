71162a00c8c974069755a7c1611c81a24f75f5d4
Jonas Metzener <jonas.metzener@adfinis-sygroup.ch>
2017-03-28 12:06:22 +0200

    Fixed the django duration serializer

--- tests/unit/transforms/django-duration-test.js
+++ tests/unit/transforms/django-duration-test.js
@@ -3,9 +3,11 @@ import { setupTest }    from 'ember-mocha'
 import { expect }       from 'chai'
 import moment           from 'moment'
 
-const HOUR   = 3600
-const MINUTE = 60
-const SECOND = 1
+const MILLISECOND = 1 / 1000
+const SECOND      = MILLISECOND * 1000
+const MINUTE      = SECOND * 60
+const HOUR        = MINUTE * 60
+const DAY         = HOUR * 24
 
 describe('Unit | Transform | django duration', function() {
   setupTest('transform:django-duration', {
@@ -16,43 +18,60 @@ describe('Unit | Transform | django duration', function() {
   it('serializes', function() {
     let transform = this.subject()
 
+    expect(transform.serialize(null)).to.be.null
+
     expect(
-      transform.serialize(moment.duration({ hours: 1, minutes: 2, seconds: 3 }))
+      transform.serialize(moment.duration({
+        hours: 1,
+        minutes: 2,
+        seconds: 3
+      }))
     ).to.equal(
       '01:02:03'
     )
 
     expect(
-      transform.serialize(moment.duration({ hours: 100, minutes: 2, seconds: 3 }))
+      transform.serialize(moment.duration({
+        days: 1,
+        hours: 2,
+        minutes: 3,
+        seconds: 4
+      }))
     ).to.equal(
-      '100:02:03'
-    )
-
-    expect(
-      transform.serialize(moment.duration({ hours: 100, minutes: 65, seconds: 3 }))
-    ).to.equal(
-      '101:05:03'
-    )
-
-    expect(
-      transform.serialize(moment.duration({ hours: 100, minutes: 1, seconds: 63 }))
-    ).to.equal(
-      '100:02:03'
+      '1 02:03:04'
     )
 
     expect(
       transform.serialize(moment.duration({
-        milliseconds: 50,
-        seconds: 3,
-        minutes: 2,
         hours: 1,
-        days: 1,
-        weeks: 1,
-        months: 1,
-        years: 1
+        minutes: 2,
+        seconds: 3,
+        milliseconds: 4
       }))
     ).to.equal(
-      '9697:02:03'
+      '01:02:03.004000'
+    )
+
+    expect(
+      transform.serialize(moment.duration({
+        days: 1,
+        hours: 2,
+        minutes: 3,
+        seconds: 4,
+        milliseconds: 5
+      }))
+    ).to.equal(
+      '1 02:03:04.005000'
+    )
+
+    expect(
+      transform.serialize(moment.duration({
+        hours: -1,
+        minutes: -2,
+        seconds: -3
+      }))
+    ).to.equal(
+      '-1 22:57:57'
     )
   })
 
@@ -63,27 +82,57 @@ describe('Unit | Transform | django duration', function() {
     expect(transform.deserialize(null)).to.be.null
 
     expect(
-      transform.deserialize('01:02:03').asSeconds()
+      transform.deserialize('01:02:03').asMilliseconds()
     ).to.equal(
-      1 * HOUR + 2 * MINUTE + 3 * SECOND
+      moment.duration({
+        hours: 1,
+        minutes: 2,
+        seconds: 3
+      }).asMilliseconds()
     )
 
     expect(
-      transform.deserialize('01:02:63').asSeconds()
+      transform.deserialize('1 02:03:04').asMilliseconds()
     ).to.equal(
-      1 * HOUR + 3 * MINUTE + 3 * SECOND
+      moment.duration({
+        days: 1,
+        hours: 2,
+        minutes: 3,
+        seconds: 4
+      }).asMilliseconds()
     )
 
     expect(
-      transform.deserialize('01:62:03').asSeconds()
+      transform.deserialize('01:02:03.004000').asMilliseconds()
     ).to.equal(
-      2 * HOUR + 2 * MINUTE + 3 * SECOND
+      moment.duration({
+        hours: 1,
+        minutes: 2,
+        seconds: 3,
+        milliseconds: 4
+      }).asMilliseconds()
     )
 
     expect(
-      transform.deserialize('101:02:03').asSeconds()
+      transform.deserialize('1 02:03:04.005000').asMilliseconds()
     ).to.equal(
-      101 * HOUR + 2 * MINUTE + 3 * SECOND
+      moment.duration({
+        days: 1,
+        hours: 2,
+        minutes: 3,
+        seconds: 4,
+        milliseconds: 5
+      }).asMilliseconds()
+    )
+
+    expect(
+      transform.deserialize('-1 22:57:57').asMilliseconds()
+    ).to.equal(
+      moment.duration({
+        hours: -1,
+        minutes: -2,
+        seconds: -3
+      }).asMilliseconds()
     )
   })
 })
