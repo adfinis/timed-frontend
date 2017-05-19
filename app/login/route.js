6510a7d14ad2c29079971cec09bec487a39d283a
Jonas Metzener <jonas.metzener@adfinis-sygroup.ch>
2017-05-19 19:46:01 +0200

    Nicer login

--- app/login/route.js
+++ app/login/route.js
@@ -3,8 +3,7 @@
  * @submodule timed-routes
  * @public
  */
-import Route   from 'ember-route'
-import service from 'ember-service/inject'
+import Route    from 'ember-route'
 
 /**
  * The login route
@@ -14,51 +13,4 @@ import service from 'ember-service/inject'
  * @public
  */
 export default Route.extend({
-  /**
-   * The session service
-   *
-   * @property {EmberSimpleAuth.SessionService} session
-   * @public
-   */
-  session: service('session'),
-
-  /**
-   * The notify service
-   *
-   * @property {EmberNotify.NotifyService} notify
-   * @public
-   */
-  notify: service('notify'),
-
-  /**
-   * The actions for the login route
-   *
-   * @property {Object} actions
-   * @public
-   */
-  actions: {
-    /**
-     * Authenticate the user
-     *
-     * @method authenticate
-     * @param {String} username The username
-     * @param {String} password The password
-     * @public
-     */
-    async authenticate(username, password) {
-      this.set('controller.loading', true)
-
-      try {
-        await this.get('session').authenticate('authenticator:application', { username, password })
-
-        this.store.unloadAll()
-      }
-      catch(e) {
-        this.get('notify').error('Wrong username or password')
-      }
-      finally {
-        this.set('controller.loading', false)
-      }
-    }
-  }
 })
