5504c843e65ae45abd6fb043328daf08b36f8c8e
Jonas Metzener <jonas.metzener@adfinis-sygroup.ch>
2017-06-19 11:34:53 +0200

    Add babel plugins to coverage config

--- config/coverage.js
+++ config/coverage.js
@@ -1,3 +1,8 @@
 module.exports = {
-  useBabelInstrumenter: true
+  useBabelInstrumenter: true,
+  babelPlugins: [
+    'babel-plugin-transform-async-to-generator',
+    'babel-plugin-transform-decorators-legacy',
+    'babel-plugin-transform-object-rest-spread'
+  ]
 }
