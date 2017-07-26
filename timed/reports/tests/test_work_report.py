ac23c1cfa0929c3d2928bfbdc65c339a0a17c230
Oliver Sauder <oliver.sauder@adfinis-sygroup.ch>
2017-07-26 15:57:00 +0200

    New end point providing work report export

--- timed/reports/tests/test_work_report.py
+++ timed/reports/tests/test_work_report.py
@@ -1,12 +1,31 @@
+import io
+
+import ezodf
 from django.core.urlresolvers import reverse
-from rest_framework.status import HTTP_200_OK
+from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
 
 from timed.tracking.factories import ReportFactory
 
 
 def test_work_report(auth_client):
-    ReportFactory.create_batch(10)
+    user = auth_client.user
+    ReportFactory.create_batch(10, user=user)
 
     url = reverse('work-reports-list')
-    res = auth_client.get(url)
+    res = auth_client.get(url, data={
+        'user': auth_client.user.id
+    })
     assert res.status_code == HTTP_200_OK
+
+    content = io.BytesIO(res.content)
+    doc = ezodf.opendoc(content)
+    table = doc.sheets[0]
+    assert table['B9'].value == 'Test User'
+
+
+def test_work_report_empty(auth_client):
+    url = reverse('work-reports-list')
+    res = auth_client.get(url, data={
+        'user': auth_client.user.id
+    })
+    assert res.status_code == HTTP_400_BAD_REQUEST
--- timed/reports/views.py
+++ timed/reports/views.py
@@ -1,6 +1,12 @@
-from django.http import HttpResponse
+from datetime import date
+from io import BytesIO
+
+import ezodf
+from django.http import HttpResponse, HttpResponseBadRequest
+from pkg_resources import resource_string
 from rest_framework.viewsets import GenericViewSet
 
+from timed.projects.models import Customer, Project
 from timed.tracking.filters import ReportFilterSet
 from timed.tracking.models import Report
 
@@ -14,15 +20,80 @@ class WorkReport(GenericViewSet):
     """
 
     filter_class = ReportFilterSet
-    ordering_fields = ('-date', )
+    ordering = ('-date', )
 
     def get_queryset(self):
         return Report.objects.select_related(
-            'task',
             'user',
-            'activity'
         )
+
+    def _parse_query_params(self, queryset, request):
+        """Parse query params by using filter_class."""
+        fltr = self.filter_class(
+            request.query_params,
+            queryset=queryset,
+            request=request)
+        form = fltr.form
+        form.is_valid()
+        return form.cleaned_data
+
+    def _format_user_name(self, user):
+        # in the long run this function should move onto User in timed
+        first_name = user.first_name or ''
+        last_name = user.last_name or ''
+
+        return '{0} {1}'.format(first_name, last_name).strip()
 
     def list(self, request, *args, **kwargs):
         queryset = self.filter_queryset(self.get_queryset())
-        return HttpResponse(queryset.count())
+        count = queryset.count()
+        if count == 0:
+            return HttpResponseBadRequest()
+        params = self._parse_query_params(queryset, request)
+
+        customer = Customer.objects.filter(id=params.get('customer')).first()
+        project = Project.objects.filter(id=params.get('project')).first()
+        from_date = params.get('from_date')
+        to_date = params.get('to_date')
+
+        tmpl = resource_string('timed_adfinis.reporting', 'workreport.ots')
+        doc = ezodf.opendoc(BytesIO(tmpl))
+        table = doc.sheets[0]
+        date_style = table['B5'].style_name
+        # in template cell C3 is empty but styled as needed for float
+        float_style = table['C3'].style_name
+
+        def _set_value(cell, value, style_name=None, value_type=None):
+            if value is not None:
+                table[cell].set_value(value, value_type=value_type)
+                if style_name is not None:
+                    table[cell].style_name = date_style
+
+        # header values
+        _set_value('B3', customer and customer.name)
+        _set_value('B4', project and project.name)
+        _set_value('B5', from_date, date_style, 'date')
+        _set_value('B6', to_date, date_style, 'date')
+        _set_value('B8', date.today(), date_style, 'date')
+        _set_value('B9', self._format_user_name(request.user))
+
+        # for simplicity insert reports in reverse order
+        for report in queryset:
+            table.insert_rows(12, 1)
+            _set_value('A13', report.date, date_style, 'date')
+
+            hours = report.duration.total_seconds() / 60 / 60
+            _set_value('B13', hours, float_style)
+
+            _set_value('C13', self._format_user_name(report.user))
+            _set_value('D13', report.comment)
+
+        # calculate location of total hours as insert rows moved it
+        table[13 + count, 2].formula = 'of:=SUM(B13:B{0})'.format(
+            str(13 + count - 1))
+
+        response = HttpResponse(
+            doc.tobytes(),
+            content_type='application/vnd.oasis.opendocument.spreadsheet')
+        response['Content-Disposition'] = 'attachment; filename=workreport.ods'
+        return response
