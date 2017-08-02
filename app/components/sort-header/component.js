23deab6d49b10b1967f48fc1c315dbcc084fd45d
Christian Zosel <christian.zosel@adfinis-sygroup.ch>
2017-08-02 14:35:29 +0200

    Add ordering for analysis table

---
+++ app/components/sort-header/component.js
@@ -0,0 +1,37 @@
+import Component from 'ember-component'
+
+import computed, { readOnly } from 'ember-computed-decorators'
+
+const getDirection = state => {
+  return state.startsWith('-') ? 'desc' : 'asc'
+}
+
+const getColname = state => (state.startsWith('-') ? state.substring(1) : state)
+
+export default Component.extend({
+  tagName: 'th',
+
+  @readOnly
+  @computed('current')
+  direction(current) {
+    return getDirection(current)
+  },
+
+  @readOnly
+  @computed('current')
+  active() {
+    let by = this.get('by')
+    let current = this.get('current')
+
+    return getColname(current) === by
+  },
+
+  click() {
+    let current = this.get('current')
+    let by = this.get('by')
+    let sort =
+      this.get('active') && getDirection(current) === 'desc' ? by : `-${by}`
+
+    this.get('update')(sort)
+  }
+})
