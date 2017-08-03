dec43376c7220304d66a7206a604e2f19ef97139
Oliver Sauder <oliver.sauder@adfinis-sygroup.ch>
2017-08-03 17:32:00 +0200

    Add support subscriptions for parity with SSA portal
    These includes:
    * customer password
    * subscription and packages
    * orders
    * import from timescout

--- timed/subscription/admin.py
+++ timed/subscription/admin.py
@@ -1,8 +1,14 @@
-"""Views for the admin interface."""
+import hashlib
 
+from django import forms
 from django.contrib import admin
 
+from timed.projects.admin import CustomerAdmin
+from timed.projects.models import Customer
+
 from . import models
+
+admin.site.unregister(Customer)
 
 
 @admin.register(models.Subscription)
@@ -19,9 +25,26 @@ class PackageAdmin(admin.ModelAdmin):
     list_display = ['subscription', 'duration', 'price']
 
 
-@admin.register(models.SubscriptionProject)
-class SubscriptionProjectAdmin(admin.ModelAdmin):
-    """Subscription project assignment admin view."""
+class SubscriptionProjectInline(admin.StackedInline):
+    model = models.SubscriptionProject
 
-    list_display = ['project', 'subscription']
-    list_filter  = ['project']
+
+class CustomerPasswordForm(forms.ModelForm):
+    def save(self, commit=True):
+        password = self.cleaned_data.get('password')
+        if password is not None:
+            self.instance.password = hashlib.md5(
+                password.encode()).hexdigest()
+        return super().save(commit=commit)
+
+
+class CustomerPasswordInline(admin.StackedInline):
+    form = CustomerPasswordForm
+    model = models.CustomerPassword
+
+
+@admin.register(Customer)
+class CustomerAdmin(CustomerAdmin):
+    inlines = CustomerAdmin.inlines + [
+        CustomerPasswordInline
+    ]
--- timed/subscription/models.py
+++ timed/subscription/models.py
@@ -1,6 +1,7 @@
-from __future__ import unicode_literals
-
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
+from django.utils.translation import ugettext_lazy as _
 from djmoney.models.fields import MoneyField
 
 
@@ -35,6 +36,33 @@ class SubscriptionProject(models.Model):
     A project can only be assigned to one subscription.
     """
 
+    project = models.OneToOneField('projects.Project')
+    subscription = models.ForeignKey(Subscription, on_delete=models.CASCADE)
+
+
+class Order(models.Model):
+    """Order of customer for specific amount of hours."""
+
     project = models.OneToOneField('projects.Project',
+                                   on_delete=models.CASCADE,
                                    related_name='subscription')
-    subscription = models.ForeignKey(Subscription)
+    duration = models.DurationField()
+    ordered = models.DateTimeField(default=timezone.now)
+    acknowledged = models.BooleanField(default=False)
+    confirmedby = models.ForeignKey(settings.AUTH_USER_MODEL,
+                                    on_delete=models.SET_NULL,
+                                    null=True, blank=True,
+                                    related_name='orders_confirmed')
+
+
+class CustomerPassword(models.Model):
+    """
+    Password per customer used for login into SySupport portal.
+
+    Password are only hashed with md5. This model will be obsolete
+    once customer center will go live.
+    """
+
+    customer = models.OneToOneField('projects.Customer')
+    password = models.CharField(_('password'), max_length=128,
+                                null=True, blank=True)
