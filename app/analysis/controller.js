29ab1107a42c2f83686685cc8026e2aa57946ddb
Christian Zosel <christian.zosel@adfinis-sygroup.ch>
2017-08-10 10:16:48 +0200

    Add JWT token to URL for exports

--- app/analysis/controller.js
+++ app/analysis/controller.js
@@ -1,16 +1,29 @@
 import Ember from 'ember'
 import Controller from 'ember-controller'
+import service from 'ember-service/inject'
 import config from '../config/environment'
+import { oneWay } from 'ember-computed-decorators'
 
 const { testing } = Ember
 
-const appendFilters = (url, filters) => {
+const join = (url, toAppend) => {
   let joinSymbol = url.includes('?') ? '&' : '?'
-  return `${url}${joinSymbol}${Object.keys(filters)
-    .map(key => {
-      return `${key}=${filters[key]}`
-    })
-    .join('&')}`
+  return url + joinSymbol + toAppend
+}
+
+const appendFilters = (url, filters) => {
+  return join(
+    url,
+    Object.keys(filters)
+      .map(key => {
+        return `${key}=${filters[key]}`
+      })
+      .join('&')
+  )
+}
+
+const appendAuth = (url, token) => {
+  return join(url, `jwt=${token}`)
 }
 
 export default Controller.extend({
@@ -18,11 +31,26 @@ export default Controller.extend({
 
   page: 1,
 
+  session: service(),
+
+  /**
+   * The current JWT token
+   *
+   * @property {string} token
+   * @public
+   */
+  @oneWay('session.data.authenticated.token') token: null,
+
   sort: '-date',
   exportLinks: config.APP.REPORTEXPORTS,
+
+  getTarget(url, filters) {
+    return appendAuth(appendFilters(url, filters), this.get('token'))
+  },
+
   actions: {
     download(url, filters) {
-      let target = appendFilters(url, filters)
+      let target = this.getTarget(url, filters)
 
       if (testing) {
         return
