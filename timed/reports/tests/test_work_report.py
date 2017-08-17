821bbebb3896614e042328d25966a8f39d4079e2
Oliver Sauder <oliver.sauder@adfinis-sygroup.ch>
2017-08-17 09:59:52 +0200

    Create one work report per project
    when filters result in several projects zip different work reports.

--- timed/reports/tests/test_work_report.py
+++ timed/reports/tests/test_work_report.py
@@ -1,26 +1,81 @@
 import io
+from datetime import date
+from zipfile import ZipFile
 
 import ezodf
+import pytest
 from django.core.urlresolvers import reverse
 from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
 
+from timed.projects.factories import (CustomerFactory, ProjectFactory,
+                                      TaskFactory)
 from timed.tracking.factories import ReportFactory
 
 
-def test_work_report(auth_client):
+@pytest.mark.freeze_time('2017-09-01')
+def test_work_report_single_project(auth_client):
     user = auth_client.user
-    ReportFactory.create_batch(10, user=user)
+    # spaces should be replaced with underscore
+    customer = CustomerFactory.create(name='Customer Name')
+    # slashes should be dropped from file name
+    project = ProjectFactory.create(customer=customer, name='Project/')
+    task = TaskFactory.create(project=project)
+    ReportFactory.create_batch(
+        10, user=user, task=task, date=date(2017, 8, 17)
+    )
+
+    url = reverse('work-reports-list')
+    res = auth_client.get(url, data={
+        'user': auth_client.user.id,
+        'from_date': '2017-08-01',
+        'to_date': '2017-08-31',
+    })
+    assert res.status_code == HTTP_200_OK
+    assert '1708-20170901-Customer_Name-Project.ods' in (
+        res['Content-Disposition']
+    )
+
+    content = io.BytesIO(res.content)
+    doc = ezodf.opendoc(content)
+    table = doc.sheets[0]
+    assert table['B5'].value == '2017-08-01'
+    assert table['B6'].value == '2017-08-31'
+    assert table['B9'].value == 'Test User'
+
+
+@pytest.mark.freeze_time('2017-09-01')
+def test_work_report_multiple_projects(auth_client):
+    NUM_PROJECTS = 2
+
+    user = auth_client.user
+    customer = CustomerFactory.create(name='Customer')
+    report_date = date(2017, 8, 17)
+    for i in range(NUM_PROJECTS):
+        project = ProjectFactory.create(
+            customer=customer, name='Project{0}'.format(i)
+        )
+        task = TaskFactory.create(project=project)
+        ReportFactory.create_batch(10, user=user, task=task, date=report_date)
 
     url = reverse('work-reports-list')
     res = auth_client.get(url, data={
         'user': auth_client.user.id
     })
     assert res.status_code == HTTP_200_OK
+    assert '20170901-WorkReports.zip' in (
+        res['Content-Disposition']
+    )
 
     content = io.BytesIO(res.content)
-    doc = ezodf.opendoc(content)
-    table = doc.sheets[0]
-    assert table['B9'].value == 'Test User'
+    with ZipFile(content, 'r') as zipfile:
+        for i in range(NUM_PROJECTS):
+            ods_content = zipfile.read(
+                '1708-20170901-Customer-Project{0}.ods'.format(i)
+            )
+            doc = ezodf.opendoc(io.BytesIO(ods_content))
+            table = doc.sheets[0]
+            assert table['B5'].value == '2017-08-17'
+            assert table['B6'].value == '2017-08-17'
 
 
 def test_work_report_empty(auth_client):
--- timed/reports/views.py
+++ timed/reports/views.py
@@ -1,12 +1,14 @@
+import re
+from collections import defaultdict
 from datetime import date
 from io import BytesIO
+from zipfile import ZipFile
 
 from django.http import HttpResponse, HttpResponseBadRequest
 from ezodf import Cell, opendoc
 from pkg_resources import resource_string
 from rest_framework.viewsets import GenericViewSet
 
-from timed.projects.models import Customer, Project
 from timed.tracking.filters import ReportFilterSet
 from timed.tracking.models import Report
 
@@ -14,6 +16,9 @@ from timed.tracking.models import Report
 class WorkReport(GenericViewSet):
     """
     Build a ods work report of reports with given filters.
+
+    It creates one work report per project. If given filters results
+    in several projects work reports will be returned as zip.
 
     Work report is Adfinis specific and is used for Administration
     to send invoice to customer.
@@ -37,18 +42,40 @@ class WorkReport(GenericViewSet):
         form.is_valid()
         return form.cleaned_data
 
-    def list(self, request, *args, **kwargs):
-        queryset = self.filter_queryset(self.get_queryset())
-        count = queryset.count()
-        if count == 0:
-            return HttpResponseBadRequest()
-        params = self._parse_query_params(queryset, request)
+    def _clean_filename(self, name):
+        """
+        Clean name so it can be used in file paths.
 
-        customer = Customer.objects.filter(id=params.get('customer')).first()
-        project = Project.objects.filter(id=params.get('project')).first()
-        from_date = params.get('from_date')
-        to_date = params.get('to_date')
-        today = date.today()
+        To accomplish this it will remove all special chars and
+        replace spaces with underscores
+        """
+        escaped = re.sub('[^\w\s-]', '', name)
+        return re.sub('\s+', '_', escaped)
+
+    def _generate_workreport_name(self, from_date, today, project):
+        """
+        Generate workreport name.
+
+        Name is in format: YYMM-YYYYMMDD-$Customer-$Project.ods
+        whereas YYMM is year and month of from_date and YYYYMMDD
+        is date when work reports gets created.
+        """
+        return '{0}-{1}-{2}-{3}.ods'.format(
+            from_date.strftime('%y%m'),
+            today.strftime('%Y%m%d'),
+            self._clean_filename(project.customer.name),
+            self._clean_filename(project.name)
+        )
+
+    def _create_workreport(self, from_date, to_date, today, project, reports,
+                           user):
+        """
+        Create ods workreport.
+
+        :rtype: tuple
+        :return: tuple where as first value is name and second ezodf document
+        """
+        customer = project.customer
 
         tmpl = resource_string('timed_adfinis.reporting', 'workreport.ots')
         doc = opendoc(BytesIO(tmpl))
@@ -59,17 +86,8 @@ class WorkReport(GenericViewSet):
         # in template cell C4 is empty but styled as needed for text with wrap
         text_style = table['C4'].style_name
 
-        # header values
-        table['B3'] = Cell(customer and customer.name)
-        table['B3'] = Cell(customer and customer.name)
-        table['B4'] = Cell(project and project.name)
-        table['B5'] = Cell(from_date, style_name=date_style, value_type='date')
-        table['B6'] = Cell(to_date, style_name=date_style, value_type='date')
-        table['B8'] = Cell(today, style_name=date_style, value_type='date')
-        table['B9'] = Cell(request.user.get_full_name())
-
         # for simplicity insert reports in reverse order
-        for report in queryset:
+        for report in reports:
             table.insert_rows(12, 1)
             table['A13'] = Cell(report.date, style_name=date_style,
                                 value_type='date')
@@ -80,12 +98,66 @@ class WorkReport(GenericViewSet):
             table['C13'] = Cell(report.user.get_full_name())
             table['D13'] = Cell(report.comment, style_name=text_style)
 
-        # calculate location of total hours as insert rows moved it
-        table[13 + count, 2].formula = 'of:=SUM(B13:B{0})'.format(
-            str(13 + count - 1))
+            # when from and to date are None find lowest and biggest date
+            from_date = min(report.date, from_date or date.max)
+            to_date = max(report.date, to_date or date.min)
 
-        response = HttpResponse(
-            doc.tobytes(),
-            content_type='application/vnd.oasis.opendocument.spreadsheet')
-        response['Content-Disposition'] = 'attachment; filename=workreport.ods'
+        # header values
+        table['B3'] = Cell(customer and customer.name)
+        table['B4'] = Cell(project and project.name)
+        table['B5'] = Cell(from_date, style_name=date_style, value_type='date')
+        table['B6'] = Cell(to_date, style_name=date_style, value_type='date')
+        table['B8'] = Cell(today, style_name=date_style, value_type='date')
+        table['B9'] = Cell(user.get_full_name())
+
+        # calculate location of total hours as insert rows moved it
+        table[13 + len(reports), 2].formula = 'of:=SUM(B13:B{0})'.format(
+            str(13 + len(reports) - 1))
+
+        name = self._generate_workreport_name(from_date, today, project)
+        return (name, doc)
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        count = queryset.count()
+        if count == 0:
+            return HttpResponseBadRequest()
+        params = self._parse_query_params(queryset, request)
+
+        from_date = params.get('from_date')
+        to_date = params.get('to_date')
+        today = date.today()
+
+        reports_by_project = defaultdict(list)
+        for report in queryset:
+            reports_by_project[report.task.project].append(report)
+
+        docs = [
+            self._create_workreport(
+                from_date, to_date, today, project, reports, request.user
+            )
+            for project, reports in reports_by_project.items()
+        ]
+
+        if len(docs) == 1:
+            name, doc = docs[0]
+            response = HttpResponse(
+                doc.tobytes(),
+                content_type='application/vnd.oasis.opendocument.spreadsheet')
+            response['Content-Disposition'] = (
+                'attachment; filename=%s' % name
+            )
+            return response
+
+        # zip multiple work reports
+        buf = BytesIO()
+        with ZipFile(buf, 'w') as zf:
+            for name, doc in docs:
+                zf.writestr(name, doc.tobytes())
+        response = HttpResponse(buf.getvalue(), content_type='application/zip')
+        response['Content-Disposition'] = (
+            'attachment; filename=%s-WorkReports.zip' % (
+                today.strftime('%Y%m%d')
+            )
+        )
         return response
