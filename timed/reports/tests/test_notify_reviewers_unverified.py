5c9bdf25be822dec1980adc2e3b1f0ae452712b7
Oliver Sauder <oliver.sauder@adfinis-sygroup.ch>
2017-08-18 11:17:08 +0200

    Add management command to notify reviewers on unverified reports

---
+++ timed/reports/tests/test_notify_reviewers_unverified.py
@@ -0,0 +1,40 @@
+from datetime import date
+
+import pytest
+from django.core.management import call_command
+
+from timed.employment.factories import UserFactory
+from timed.projects.factories import ProjectFactory, TaskFactory
+from timed.tracking.factories import ReportFactory
+
+
+@pytest.mark.freeze_time('2017-8-4')
+def test_notify_reviewers(db, mailoutbox):
+    """Test time range 2017-7-1 till 2017-7-31."""
+    # a reviewer which will be notified
+    reviewer_work = UserFactory.create()
+    project_work = ProjectFactory.create()
+    project_work.reviewers.add(reviewer_work)
+    task_work = TaskFactory.create(project=project_work)
+    ReportFactory.create(date=date(2017, 7, 1), task=task_work,
+                         verified_by=None)
+
+    # a reviewer which doesn't have any unverfied reports
+    reviewer_no_work = UserFactory.create()
+    project_no_work = ProjectFactory.create()
+    project_no_work.reviewers.add(reviewer_no_work)
+    task_no_work = TaskFactory.create(project=project_no_work)
+    ReportFactory.create(date=date(2017, 7, 1), task=task_no_work,
+                         verified_by=reviewer_no_work)
+
+    call_command('notify_reviewers_unverified')
+
+    # checks
+    assert len(mailoutbox) == 1
+    mail = mailoutbox[0]
+    assert mail.to == [reviewer_work.email]
+    url = (
+        'http://localhost:4200/reschedule?from_date=2017-07-01&'
+        'to_date=2017-07-31&reviewer=%d'
+    ) % reviewer_work.id
+    assert url in mail.body
