a87d68d8c76f208d668482219b9262081c1ff070
Jonas Metzener <jonas.metzener@adfinis-sygroup.ch>
2017-08-25 13:42:40 +0200

    Added unit tests for tour

--- tests/unit/mixins/route-autostart-tour-test.js
+++ tests/unit/mixins/route-autostart-tour-test.js
@@ -1,13 +1,63 @@
-import { expect } from 'chai';
-import { describe, it } from 'mocha';
-import Ember from 'ember';
-import RouteAutostartTourMixin from 'timed/mixins/route-autostart-tour';
+import { expect } from 'chai'
+import { describe, it } from 'mocha'
+import EmberObject from 'ember-object'
+import RouteAutostartTourMixin from 'timed/mixins/route-autostart-tour'
 
 describe('Unit | Mixin | route autostart tour', function() {
   // Replace this with your real tests.
   it('works', function() {
-    let RouteAutostartTourObject = Ember.Object.extend(RouteAutostartTourMixin);
-    let subject = RouteAutostartTourObject.create();
-    expect(subject).to.be.ok;
-  });
-});
+    let RouteAutostartTourObject = EmberObject.extend(RouteAutostartTourMixin)
+    let subject = RouteAutostartTourObject.create()
+    expect(subject).to.be.ok
+  })
+
+  it('can get parent route name', function() {
+    let RouteAutostartTourObject = EmberObject.extend(RouteAutostartTourMixin)
+    let subject = RouteAutostartTourObject.create({ routeName: 'foo.bar.baz' })
+
+    expect(subject._getParentRouteName()).to.equal('foo.bar')
+
+    subject.set('routeName', 'foo')
+
+    expect(subject._getParentRouteName()).to.equal('')
+  })
+
+  it('can check if a tour is wanted', function() {
+    let RouteAutostartTourObject = EmberObject.extend(RouteAutostartTourMixin)
+    let subject = RouteAutostartTourObject.create({
+      routeName: 'foo.bar.baz',
+      autostartTour: EmberObject.create({ done: [] })
+    })
+
+    subject.set('media', {
+      isMd: false,
+      isLg: false,
+      isXl: false
+    })
+
+    expect(subject._wantsTour('test', EmberObject.create({ tourDone: false })))
+      .to.not.be.ok
+
+    subject.set('media', {
+      isMd: true,
+      isLg: false,
+      isXl: false
+    })
+
+    expect(subject._wantsTour('test', EmberObject.create({ tourDone: false })))
+      .to.be.ok
+
+    expect(subject._wantsTour('test', EmberObject.create({ tourDone: true })))
+      .to.not.be.ok
+
+    subject.set('autostartTour.done', [])
+
+    expect(subject._wantsTour('test', EmberObject.create({ tourDone: false })))
+      .to.be.ok
+
+    subject.set('autostartTour.done', ['test'])
+
+    expect(subject._wantsTour('test', EmberObject.create({ tourDone: false })))
+      .to.not.be.ok
+  })
+})
--- tests/unit/services/autostart-tour-test.js
+++ tests/unit/services/autostart-tour-test.js
@@ -1,16 +1,47 @@
-import { expect } from 'chai';
-import { describe, it } from 'mocha';
-import { setupTest } from 'ember-mocha';
+import { expect } from 'chai'
+import { describe, it, afterEach } from 'mocha'
+import { setupTest } from 'ember-mocha'
 
 describe('Unit | Service | autostart tour', function() {
-  setupTest('service:autostart-tour', {
-    // Specify the other units that are required for this test.
-    // needs: ['service:foo']
-  });
+  setupTest(
+    'service:autostart-tour',
+    {
+      // Specify the other units that are required for this test.
+      // needs: ['service:foo']
+    }
+  )
+
+  afterEach(function() {
+    localStorage.removeItem('timed-tour-test')
+  })
 
   // Replace this with your real tests.
   it('exists', function() {
-    let service = this.subject();
-    expect(service).to.be.ok;
-  });
-});
+    let service = this.subject()
+    expect(service).to.be.ok
+  })
+
+  it('can set and get the done tours', function() {
+    let service = this.subject({ doneKey: 'timed-tour-test' })
+
+    expect(service.get('done')).to.deep.equal([])
+
+    service.set('done', ['test'])
+
+    expect(service.get('done')).to.deep.equal(['test'])
+  })
+
+  it('can check if all tours are done', function() {
+    let service = this.subject({ doneKey: 'timed-tour-test', tours: ['test'] })
+
+    expect(service.allDone()).to.not.be.ok
+
+    service.set('done', ['test'])
+
+    expect(service.allDone()).to.be.ok
+
+    service.set('done', ['test', 'test2'])
+
+    expect(service.allDone()).to.be.ok
+  })
+})
