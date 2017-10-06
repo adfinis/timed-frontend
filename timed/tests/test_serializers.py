5adaa6ec7b1c379d0fcbf7b488cc48e183739f0e
Oliver Sauder <oliver.sauder@adfinis-sygroup.ch>
2017-10-06 10:00:52 +0200

    Add unit test for pk dict serializer

---
+++ timed/tests/test_serializers.py
@@ -0,0 +1,56 @@
+from datetime import timedelta
+
+import pytest
+from rest_framework_json_api.serializers import DurationField, IntegerField
+
+from timed.serializers import PkDictSerializer
+
+
+class MyPkDictSerializer(PkDictSerializer):
+    test_duration = DurationField()
+    test_nr = IntegerField()
+
+    class Meta:
+        pk_key = 'test_nr'
+        resource_name = 'my-resource'
+
+
+@pytest.fixture
+def data():
+    return {
+        'test_nr': 123,
+        'test_duration': timedelta(hours=1),
+        'invalid_field': '1234'
+    }
+
+
+def test_pk_dict_serializer_single(data):
+    serializer = MyPkDictSerializer(data)
+
+    expected_data = {
+        'test_duration': '01:00:00',
+        'test_nr': 123,
+    }
+
+    assert expected_data == serializer.data
+
+
+def test_pk_dict_serializer_many(data):
+    list_data = [
+        data,
+        data
+    ]
+    serializer = MyPkDictSerializer(list_data, many=True)
+
+    expected_data = [
+        {
+            'test_duration': '01:00:00',
+            'test_nr': 123,
+        },
+        {
+            'test_duration': '01:00:00',
+            'test_nr': 123,
+        },
+    ]
+
+    assert expected_data == serializer.data
