9d78707884dc619b51fb71e8c4a44e8a61a91aef
Oliver Sauder <oliver.sauder@adfinis-sygroup.ch>
2017-10-20 09:19:18 +0200

    Add statistic tests

---
+++ timed/reports/tests/test_user_statistic.py
@@ -0,0 +1,57 @@
+from datetime import timedelta
+
+from django.core.urlresolvers import reverse
+
+from timed.tracking.factories import ReportFactory
+
+
+def test_user_statistic_list(auth_client):
+    user = auth_client.user
+    ReportFactory.create(duration=timedelta(hours=1), user=user)
+    ReportFactory.create(duration=timedelta(hours=2), user=user)
+    report = ReportFactory.create(duration=timedelta(hours=2))
+
+    url = reverse('user-statistic-list')
+    result = auth_client.get(url, data={
+        'ordering': 'duration',
+        'include': 'user'
+    })
+    assert result.status_code == 200
+
+    json = result.json()
+    expected_json = [
+        {
+            'type': 'user-statistics',
+            'id': str(report.user.id),
+            'attributes': {
+                'duration': '02:00:00'
+            },
+            'relationships': {
+                'user': {
+                    'data': {
+                        'id': str(report.user.id),
+                        'type': 'users'
+                    }
+                }
+            }
+        },
+        {
+            'type': 'user-statistics',
+            'id': str(user.id),
+            'attributes': {
+                'duration': '03:00:00'
+            },
+            'relationships': {
+                'user': {
+                    'data': {
+                        'id': str(user.id),
+                        'type': 'users'
+                    }
+                }
+            }
+        }
+    ]
+
+    assert json['data'] == expected_json
+    assert len(json['included']) == 2
+    assert json['meta']['total-time'] == '05:00:00'
