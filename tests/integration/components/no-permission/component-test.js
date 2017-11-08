43ca56264c6c365464126b3c5341e34037ef1230
Jonas Metzener <jonas.metzener@adfinis-sygroup.ch>
2017-11-08 11:32:12 +0100

    Migrate no-permission template to a component

---
+++ tests/integration/components/no-permission/component-test.js
@@ -0,0 +1,18 @@
+import { expect } from 'chai'
+import { describe, it } from 'mocha'
+import { setupComponentTest } from 'ember-mocha'
+import hbs from 'htmlbars-inline-precompile'
+import { find } from 'ember-native-dom-helpers'
+
+describe('Integration | Component | no permission', function() {
+  setupComponentTest('no-permission', {
+    integration: true
+  })
+
+  it('renders', function() {
+    this.render(hbs`{{no-permission}}`)
+
+    expect(find('.empty')).to.have.length(1)
+    expect(find('.empty').innerHTML).to.contain('Halt')
+  })
+})
