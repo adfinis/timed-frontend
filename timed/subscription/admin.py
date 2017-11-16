0c2a7bfebbb6d427ffea66f4a8df534c5b8be974
Oliver Sauder <oliver.sauder@adfinis-sygroup.ch>
2017-11-16 14:36:27 +0100

    Configure duration field on subscription package in hours

--- timed/subscription/admin.py
+++ timed/subscription/admin.py
@@ -1,8 +1,21 @@
+from django import forms
 from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
+
+from timed.forms import DurationInHoursField
 
 from . import models
+
+
+class PackageForm(forms.ModelForm):
+    model = models.Package
+    duration = DurationInHoursField(
+        label=_('Duration in hours'),
+        required=True,
+    )
 
 
 @admin.register(models.Package)
 class PackageAdmin(admin.ModelAdmin):
     list_display = ['billing_type', 'duration', 'price']
+    form = PackageForm
