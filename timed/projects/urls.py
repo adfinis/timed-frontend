716db7fb137670567fd5ecd367b49c157baa1838
Oliver Sauder <oliver.sauder@adfinis-sygroup.ch>
2017-11-29 15:41:52 +0100

    Move from DefaultRouter to SimpleRouter
    DefaultRouter has a feature to enable root view. This only works though
    if a single DefaultRouter is used. This is not desired in our case\
    so root view was not complete.
    
    To not confuse it is therefore better to move to SimpleRouter.

--- timed/projects/urls.py
+++ timed/projects/urls.py
@@ -1,11 +1,11 @@
 """URL to view mapping for the projects app."""
 
 from django.conf import settings
-from rest_framework.routers import DefaultRouter
+from rest_framework.routers import SimpleRouter
 
 from timed.projects import views
 
-r = DefaultRouter(trailing_slash=settings.APPEND_SLASH)
+r = SimpleRouter(trailing_slash=settings.APPEND_SLASH)
 
 r.register(r'projects',  views.ProjectViewSet,  'project')
 r.register(r'customers', views.CustomerViewSet, 'customer')
--- timed/tracking/urls.py
+++ timed/tracking/urls.py
@@ -1,11 +1,11 @@
 """URL to view mapping for the tracking app."""
 
 from django.conf import settings
-from rest_framework.routers import DefaultRouter
+from rest_framework.routers import SimpleRouter
 
 from timed.tracking import views
 
-r = DefaultRouter(trailing_slash=settings.APPEND_SLASH)
+r = SimpleRouter(trailing_slash=settings.APPEND_SLASH)
 
 r.register(r'activities',      views.ActivityViewSet,      'activity')
 r.register(r'activity-blocks', views.ActivityBlockViewSet, 'activity-block')
