493f37905c9fa03d655384fd3af14eb129df0fb8
Jonas Metzener <jonas.metzener@adfinis-sygroup.ch>
2017-12-12 14:35:54 +0100

    Add missing docstring

--- app/utils/query-params.js
+++ app/utils/query-params.js
@@ -1,12 +1,26 @@
 import { get } from '@ember/object'
 import { underscore } from '@ember/string'
 
+/**
+ * Underscore all object keys
+ */
 export const underscoreQueryParams = params => {
   return Object.keys(params).reduce((obj, key) => {
     return { ...obj, [underscore(key)]: get(params, key) }
   }, {})
 }
 
+/**
+ * Serialize a query param object with a given ember-parachute QueryParams object
+ *
+ * let queryParamsObject = new QueryParams({
+ *   foo: {
+ *     serialize: val => `test-${val}`
+ *   }
+ * })
+ *
+ * serializeParachuteQueryParams({ foo: 'bar' }).foo // will be 'test-bar'
+ */
 export const serializeParachuteQueryParams = (params, queryParamsObject) => {
   return Object.keys(params).reduce((parsed, key) => {
     let serializeFn = get(queryParamsObject, `queryParams.${key}.serialize`)
