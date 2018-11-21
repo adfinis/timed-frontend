62297aa80d4bde0ccc49fb52e887090894dc114f
aziiee <akanksh1997@hotmail.com>
2018-11-21 15:14:14 +0100

    Reports are now being queried with async/await instead of promise. Other corrections including a better usage of hooks and code indentation

--- app/components/review-warning/component.js
+++ app/components/review-warning/component.js
@@ -6,29 +6,28 @@ export default Component.extend({
   isVisible: false,
   session: service('session'),
   store: service('store'),
-  startOfMonth: moment()
-    .startOf('month')
-    .subtract(1, 'months'),
-  endOfMonth: moment()
-    .endOf('month')
-    .subtract(1, 'months'),
+  startOfMonth: null,
+  endOfMonth: null,
 
-  init() {
+  async init() {
     this._super(...arguments)
+    // Subtraction is needed because the review warning is only displayed for entries of the last month
+    this.startOfMonth = moment()
+      .startOf('month')
+      .subtract(1, 'months')
+    this.endOfMonth = moment()
+      .endOf('month')
+      .subtract(1, 'months')
 
-    // Get reports
-    this.get('store')
-      .query('report', {
-        from_date: this.startOfMonth.format('YYYY-MM-DD'),
-        to_date: this.endOfMonth.format('YYYY-MM-DD'),
-        reviewer: this.get('session.data.user.id'),
-        editable: 1
-      })
-      .then(reports => {
-        // Hide review warning button if no reports found
-        if (reports.get('length') > 0) {
-          this.set('isVisible', true)
-        }
-      })
+    let reports = await this.get('store').query('report', {
+      from_date: this.startOfMonth.format('YYYY-MM-DD'),
+      to_date: this.endOfMonth.format('YYYY-MM-DD'),
+      reviewer: this.get('session.data.user.id'),
+      editable: 1
+    })
+
+    if (reports.get('length') > 0) {
+      this.set('isVisible', true)
+    }
   }
 })
