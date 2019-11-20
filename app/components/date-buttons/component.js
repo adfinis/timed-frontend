7b05c09246f72713a4a8f80ad7088afb10e20647
Akanksh Saxena <akanksh.saxena@adfinis-sygroup.ch>
2019-11-20 09:40:42 +0100

    Add additional filter options for reports
    This will allow to filter reports by last week, last month and last
    year in Analysis and Statistics.

--- app/components/date-buttons/component.js
+++ app/components/date-buttons/component.js
@@ -4,20 +4,66 @@ import moment from 'moment'
 export default Component.extend({
   init() {
     this._super(...arguments)
-    this.set('choices', ['this week', 'this month', 'this year'])
+    this.set('choices', [
+      'this week',
+      'this month',
+      'this year',
+      'last week',
+      'last month',
+      'last year'
+    ])
   },
 
   actions: {
     selectDate(expr) {
       switch (expr) {
         case 'this week':
-          this.get('presetDate')(moment().day(1))
+          this.get('onUpdateFromDate')(moment().day(1))
+          this.get('onUpdateToDate')(null)
           break
         case 'this month':
-          this.get('presetDate')(moment().date(1))
+          this.get('onUpdateFromDate')(moment().date(1))
+          this.get('onUpdateToDate')(null)
           break
         case 'this year':
-          this.get('presetDate')(moment().dayOfYear(1))
+          this.get('onUpdateFromDate')(moment().dayOfYear(1))
+          this.get('onUpdateToDate')(null)
+          break
+        case 'last week':
+          this.get('onUpdateFromDate')(
+            moment()
+              .subtract(1, 'week')
+              .day(1)
+          )
+          this.get('onUpdateToDate')(
+            moment()
+              .subtract(1, 'week')
+              .day(7)
+          )
+          break
+        case 'last month':
+          this.get('onUpdateFromDate')(
+            moment()
+              .subtract(1, 'month')
+              .startOf('month')
+          )
+          this.get('onUpdateToDate')(
+            moment()
+              .subtract(1, 'month')
+              .endOf('month')
+          )
+          break
+        case 'last year':
+          this.get('onUpdateFromDate')(
+            moment()
+              .subtract(1, 'year')
+              .startOf('year')
+          )
+          this.get('onUpdateToDate')(
+            moment()
+              .subtract(1, 'year')
+              .endOf('year')
+          )
           break
       }
     }
