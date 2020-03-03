57ce83459572474cb4f4a38221b44624877ede27
Akanksh Saxena <akanksh.saxena@adfinis-sygroup.ch>
2020-03-03 19:42:50 +0100

    Extend view to allow selection of projects and tasks

--- app/projects/controller.js
+++ app/projects/controller.js
@@ -7,10 +7,27 @@ export default Controller.extend({
   session: service(),
   user: reads("session.data.user"),
   projects: reads("fetchProjectsOfUser.lastSuccessful.value"),
+  ordering: 1,
 
   fetchProjectsOfUser: task(function*() {
     return yield this.store.query("project", {
       reviewer: this.user.id
     });
-  })
+  }),
+
+  fetchTasksOfProject: task(function*(project) {
+    return yield this.store
+      .query("task", {
+        project: project.id
+      })
+      .toArray();
+  }).drop(),
+
+  saveTask: task(function*() {}),
+
+  actions: {
+    onSelectTask(task) {
+      this.set("selectedTask", task);
+    }
+  }
 });
