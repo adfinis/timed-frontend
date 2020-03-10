dc735eb2f6d347578e7e02ec13b87e173c7c365b
Akanksh Saxena <akanksh.saxena@adfinis-sygroup.ch>
2020-03-10 14:32:40 +0100

    Add validation for task and improve error handling

--- app/projects/controller.js
+++ app/projects/controller.js
@@ -2,38 +2,66 @@ import Controller from "@ember/controller";
 import { reads } from "@ember/object/computed";
 import { inject as service } from "@ember/service";
 import { task } from "ember-concurrency";
+import TaskValidations from "timed/validations/task";
 
 export default Controller.extend({
+  TaskValidations,
+
   session: service(),
+  notify: service(),
   user: reads("session.data.user"),
   projects: reads("fetchProjectsOfUser.lastSuccessful.value"),
   tasks: reads("fetchTasksOfProject.lastSuccessful.value"),
   ordering: 1,
 
   fetchProjectsOfUser: task(function*() {
-    return yield this.store.query("project", {
-      reviewer: this.user.id
-    });
+    try {
+      return yield this.store.query("project", {
+        reviewer: this.user.id
+      });
+    } catch (exception) {
+      this.notify.error("Could not fetch projects");
+    }
   }),
 
-  fetchTasksOfProject: task(function*(project) {
-    return yield this.store.query("task", {
-      project: project.id
-    });
+  fetchTasksOfProject: task(function*() {
+    try {
+      return yield this.store.query("task", {
+        project: this.get("selectedProject.id")
+      });
+    } catch (exception) {
+      this.notify.error("Could not fetch tasks");
+    }
   }).drop(),
 
   saveTask: task(function*(changeset) {
-    yield changeset.save();
+    try {
+      yield changeset.save();
+
+      this.notify.success("Task was saved");
+    } catch (exception) {
+      this.notify.error("Could not save task");
+    }
 
     this.fetchTasksOfProject.perform(this.get("selectedProject"));
   }).drop(),
 
   createTask: task(function*() {
-    this.set(
-      "selectedTask",
-      yield this.store.createRecord("task", {
-        project: this.get("selectedProject")
-      })
-    );
-  }).drop()
+    try {
+      this.set(
+        "selectedTask",
+        yield this.store.createRecord("task", {
+          project: this.get("selectedProject")
+        })
+      );
+    } catch (exception) {
+      this.notify.error("Could not add task");
+    }
+  }).drop(),
+
+  actions: {
+    clearProjectSelection() {
+      this.set("selectedProject", null);
+    }
+  }
 });
