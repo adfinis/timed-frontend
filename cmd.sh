a6aac24dbb69636b39328f0b086ecbbcf7b81856
Michael Imfeld <michael.imfeld@adfinis.com>
2020-07-16 10:17:54 +0200

    Fix sed in cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-sed -i 's/max-requests = .*/max-requests = '"${UWSGI_MAX_REQUESTS}"'/g' \
-    -i 's/harakiri = .*/harakiri = '"${UWSGI_HARAKIRI}"'/g' \
-    -i 's/processes = .*/processes = '"${UWSGI_PROCESSES}"'/g' "${UWSGI_INI}"
+sed -i \
+    -e 's/max-requests = .*/max-requests = '"${UWSGI_MAX_REQUESTS}"'/g' "${UWSGI_INI}" \
+    -e 's/harakiri = .*/harakiri = '"${UWSGI_HARAKIRI}"'/g' "${UWSGI_INI}" \
+    -e 's/processes = .*/processes = '"${UWSGI_PROCESSES}"'/g' "${UWSGI_INI}"
 
 wait-for-it.sh "${DJANGO_DATABASE_HOST}":"${DJANGO_DATABASE_PORT}" -t "${WAITFORIT_TIMEOUT}" -- ./manage.py migrate && uwsgi
