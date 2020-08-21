39f74fee7ca5af0d2673a995981d61372298ee7b
Akanksh Saxena <akanksh1997@hotmail.com>
2020-08-21 10:53:55 +0200

    Revert "fix(auth): Redirect to SSO if authentication fails"

--- app/protected/route.js
+++ app/protected/route.js
@@ -25,20 +25,14 @@ export default Route.extend(AuthenticatedRouteMixin, {
   media: service("media"),
   store: service(),
   ajax: service(),
-  router: service(),
 
   async model() {
-    let user;
-    try {
-      user = await this.ajax.request("/api/v1/users/me", {
-        method: "GET",
-        data: {
-          include: "supervisors,supervisees"
-        }
-      });
-    } catch (error) {
-      this.router.transitionTo("login");
-    }
+    const user = await this.ajax.request("/api/v1/users/me", {
+      method: "GET",
+      data: {
+        include: "supervisors,supervisees"
+      }
+    });
 
     await this.store.pushPayload("user", user);
 
