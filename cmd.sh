50dcbacfb00c85811a349c50ba7d1fffe60f9de1
Marc-Aurèle Brothier <marc-aurele.brothier@adfinis.com>
2020-11-23 16:04:20 +0100

    cmd: ensure no input will be asked for Django migration

--- cmd.sh
+++ cmd.sh
@@ -5,4 +5,4 @@ sed -i \
     -e 's/harakiri = .*/harakiri = '"${UWSGI_HARAKIRI}"'/g' "${UWSGI_INI}" \
     -e 's/processes = .*/processes = '"${UWSGI_PROCESSES}"'/g' "${UWSGI_INI}"
 
-wait-for-it.sh "${DJANGO_DATABASE_HOST}":"${DJANGO_DATABASE_PORT}" -t "${WAITFORIT_TIMEOUT}" -- ./manage.py migrate && uwsgi
+wait-for-it.sh "${DJANGO_DATABASE_HOST}":"${DJANGO_DATABASE_PORT}" -t "${WAITFORIT_TIMEOUT}" -- ./manage.py migrate --no-input && uwsgi
