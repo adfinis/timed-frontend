186fff0d76b161f48a4c40dec627a1c8fac6e94a
Wiktork <wiktor.kepczynski@adfinis.com>
2021-04-09 16:00:17 +0200

    feat: visualise  customer visible projects
    add an icon to the comment field for customer visible projects

---
+++ tests/integration/components/customer-visible-icon/component-test.js
@@ -0,0 +1,14 @@
+import { render } from "@ember/test-helpers";
+import { setupRenderingTest } from "ember-qunit";
+import hbs from "htmlbars-inline-precompile";
+import { module, test } from "qunit";
+
+module("Integration | Component | customer visible icon", function(hooks) {
+  setupRenderingTest(hooks);
+
+  test("renders", async function(assert) {
+    await render(hbs`{{customer-visible-icon}}`);
+
+    assert.dom("i.fa.fa-eye").exists();
+  });
+});
