8a2abc759e73e963e988314293708205208fb5e5
David Vogt <david.vogt@adfinis.com>
2021-05-04 18:56:49 +0200

    chore(deployment): collectstatic can run before db connection check
    This way, we waste a little less time, as `collectstatic` can run
    without having the DB available.

--- cmd.sh
+++ cmd.sh
@@ -2,7 +2,8 @@
 
 GUNICORN_WORKERS="${GUNICORN_WORKERS:-8}"
 
+./manage.py collectstatic --noinput
+
 wait-for-it.sh "${DJANGO_DATABASE_HOST}":"${DJANGO_DATABASE_PORT}" -t "${WAITFORIT_TIMEOUT}" -- \
     ./manage.py migrate --no-input && \
-    ./manage.py collectstatic --noinput && \
     gunicorn --workers=$GUNICORN_WORKERS --bind=0.0.0.0:80 timed.wsgi:application
