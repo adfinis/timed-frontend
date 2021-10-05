96179d03a10707ef455126a4f66517cb1ccf0c35
Akanksh Saxena <akanksh.saxena@adfinis-sygroup.ch>
2021-10-05 09:43:37 +0200

    fix(analysis): do not allow to edit reports once they are verified
    This fixes the issue where the reports had to be verified AND billed in order to
    make them non-editable. The reports should not be editable once
    they are verified OR billed.

--- app/abilities/report.js
+++ app/abilities/report.js
@@ -8,7 +8,7 @@ export default Ability.extend({
     function() {
       const isEditable =
         this.get("user.isSuperuser") ||
-        (!(this.get("model.verifiedBy.id") && this.get("model.billed")) &&
+        (!(this.get("model.verifiedBy.id") || this.get("model.billed")) &&
           (this.get("model.user.id") === this.get("user.id") ||
             (this.get("model.user.supervisors") ?? [])
               .mapBy("id")
