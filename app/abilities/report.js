1b0d3e10bbb8e5a25066df6a1717734e236ff604
Wiktork <wiktor.kepczynski@adfinis.com>
2021-10-06 13:16:47 +0200

    fix(ability): can't edit reports if verified and reviewer

--- app/abilities/report.js
+++ app/abilities/report.js
@@ -8,18 +8,19 @@ export default Ability.extend({
     function() {
       const isEditable =
         this.get("user.isSuperuser") ||
-        (!(this.get("model.verifiedBy.id") || this.get("model.billed")) &&
+        (!this.get("model.verifiedBy.id") &&
           (this.get("model.user.id") === this.get("user.id") ||
             (this.get("model.user.supervisors") ?? [])
               .mapBy("id")
               .includes(this.get("user.id"))));
-      const isReviewer = (this.get("model.taskAssignees") ?? [])
-        .concat(
-          this.get("model.projectAssignees") ?? [],
-          this.get("model.customerAssignees") ?? []
-        )
-        .mapBy("user.id")
-        .includes(this.get("user.id"));
+      const isReviewer =
+        (this.get("model.taskAssignees") ?? [])
+          .concat(
+            this.get("model.projectAssignees") ?? [],
+            this.get("model.customerAssignees") ?? []
+          )
+          .mapBy("user.id")
+          .includes(this.get("user.id")) && !this.get("model.verifiedBy.id");
       return isEditable || isReviewer;
     }
   )
