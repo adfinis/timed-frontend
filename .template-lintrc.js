6de6186030af783f887b22a181aa02a7ea4419dc
Akanksh Saxena <akanksh.saxena@adfinis-sygroup.ch>
2021-11-22 14:09:06 +0100

    chore: update ember to v3.15
    Prepare for octane upgrade

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -1,10 +1,5 @@
 "use strict";
 
 module.exports = {
-  extends: "recommended",
-
-  rules: {
-    // https://github.com/ember-template-lint/ember-template-lint/issues/509
-    "attribute-indentation": false
-  }
+  extends: 'octane'
 };
