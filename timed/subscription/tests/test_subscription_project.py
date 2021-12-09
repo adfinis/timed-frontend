0f8d7b1a4658cf28a1451092733fc73e22a3beaa
Wiktork <wiktor.kepczynski@adfinis.com>
2021-12-09 17:47:20 +0100

    fix(subscription): fixed visibility on subscription-projects for customers updated tests accordingly

--- timed/subscription/tests/test_subscription_project.py
+++ timed/subscription/tests/test_subscription_project.py
@@ -1,10 +1,13 @@
 from datetime import timedelta
 
+import pytest
 from django.urls import reverse
-from rest_framework.status import HTTP_200_OK
+from rest_framework.status import HTTP_200_OK, HTTP_404_NOT_FOUND
 
+from timed.employment.factories import EmploymentFactory
 from timed.projects.factories import (
     BillingTypeFactory,
+    CustomerAssigneeFactory,
     CustomerFactory,
     ProjectFactory,
     TaskFactory,
@@ -13,7 +16,12 @@ from timed.subscription.factories import OrderFactory, PackageFactory
 from timed.tracking.factories import ReportFactory
 
 
-def test_subscription_project_list(auth_client):
+@pytest.mark.parametrize("is_external, expected", [(True, 0), (False, 1)])
+def test_subscription_project_list(auth_client, is_external, expected):
+    employment = EmploymentFactory.create(user=auth_client.user, is_external=False)
+    if is_external:
+        employment.is_external = True
+        employment.save()
     customer = CustomerFactory.create()
     billing_type = BillingTypeFactory()
     project = ProjectFactory.create(
@@ -46,21 +54,69 @@ def test_subscription_project_list(auth_client):
     assert res.status_code == HTTP_200_OK
 
     json = res.json()
-    assert len(json["data"]) == 1
-    assert json["data"][0]["id"] == str(project.id)
+    assert len(json["data"]) == expected
+    if expected:
+        assert json["data"][0]["id"] == str(project.id)
 
-    attrs = json["data"][0]["attributes"]
-    assert attrs["spent-time"] == "05:00:00"
-    assert attrs["purchased-time"] == "06:00:00"
+        attrs = json["data"][0]["attributes"]
+        assert attrs["spent-time"] == "05:00:00"
+        assert attrs["purchased-time"] == "06:00:00"
 
 
-def test_subscription_project_detail(auth_client):
+@pytest.mark.parametrize(
+    "is_customer, project_of_customer, has_employment, is_external, expected",
+    [
+        (True, True, False, False, HTTP_200_OK),
+        (True, False, False, False, HTTP_404_NOT_FOUND),
+        (False, False, True, False, HTTP_200_OK),
+        (False, False, True, True, HTTP_404_NOT_FOUND),
+    ],
+)
+def test_subscription_project_detail(
+    auth_client, is_customer, project_of_customer, has_employment, is_external, expected
+):
+    user = auth_client.user
     billing_type = BillingTypeFactory()
     project = ProjectFactory.create(billing_type=billing_type, customer_visible=True)
     PackageFactory.create_batch(2, billing_type=billing_type)
 
+    if has_employment:
+        employment = EmploymentFactory.create(user=user, is_external=False)
+        if is_external:
+            employment.is_external = True
+            employment.save()
+
+    if is_customer:
+        customer_assignee = CustomerAssigneeFactory(user=user, is_customer=True)
+        if project_of_customer:
+            customer_assignee.customer = project.customer
+            customer_assignee.save()
+
     url = reverse("subscription-project-detail", args=[project.id])
     res = auth_client.get(url)
-    assert res.status_code == HTTP_200_OK
-    json = res.json()
-    assert json["data"]["id"] == str(project.id)
+    assert res.status_code == expected
+
+    if expected == HTTP_200_OK:
+        json = res.json()
+        assert json["data"]["id"] == str(project.id)
+
+
+def test_subscription_project_list_user_is_customer(auth_client):
+    customer = CustomerFactory.create()
+    project = ProjectFactory.create(customer=customer, customer_visible=True)
+    ProjectFactory.create_batch(4, customer_visible=True)
+
+    user = auth_client.user
+    CustomerAssigneeFactory.create(user=user, customer=customer, is_customer=True)
+
+    url = reverse("subscription-project-list")
+
+    response = auth_client.get(url)
+    assert response.status_code == HTTP_200_OK
+
+    json = response.json()
+    assert len(json["data"]) == 1
+    assert json["data"][0]["id"] == str(project.id)
+    assert json["data"][0]["relationships"]["customer"]["data"]["id"] == str(
+        customer.id
+    )
