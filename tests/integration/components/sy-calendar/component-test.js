21c338786f6cf03c93cd47bd86fe4817a766e931
Falk <falk.neumann@adfinis.com>
2022-10-11 21:20:55 +0200

    refactor(sy-calendar): refactor to octane

--- tests/integration/components/sy-calendar/component-test.js
+++ tests/integration/components/sy-calendar/component-test.js
@@ -1,21 +1,24 @@
-import { hbs } from 'ember-cli-htmlbars';
 import { fillIn, render } from "@ember/test-helpers";
+import { hbs } from "ember-cli-htmlbars";
 import { setupRenderingTest } from "ember-qunit";
 import moment from "moment";
 import { module, test } from "qunit";
 
-module("Integration | Component | sy calendar", function(hooks) {
+module("Integration | Component | sy calendar", function (hooks) {
   setupRenderingTest(hooks);
 
-  test("can select a year", async function(assert) {
+  test("can select a year", async function (assert) {
     assert.expect(1);
     this.set("center", moment({ y: 2017, m: 10, d: 7 }));
+    this.set("onCenterChangeHandler", (value) => {
+      this.center = value.moment;
+    });
 
     await render(hbs`
-      {{sy-calendar
-        center=center
-        onCenterChange=(action (mut center) value='moment')
-      }}
+      <SyCalendar
+        @center={{this.center}}
+        @onCenterChange={{this.onCenterChangeHandler}}
+      />
     `);
 
     await fillIn(".nav-select-year select", "2010");
@@ -23,15 +26,18 @@ module("Integration | Component | sy calendar", function(hooks) {
     assert.strictEqual(this.center.year(), 2010);
   });
 
-  test("can select a month", async function(assert) {
+  test("can select a month", async function (assert) {
     assert.expect(1);
     this.set("center", moment({ y: 2017, m: 10, d: 7 }));
+    this.set("onCenterChangeHandler", (value) => {
+      this.center = value.moment;
+    });
 
     await render(hbs`
-      {{sy-calendar
-        center=center
-        onCenterChange=(action (mut center) value='moment')
-      }}
+      <SyCalendar
+        @center={{this.center}}
+        @onCenterChange={{this.onCenterChangeHandler}}
+      />
     `);
 
     await fillIn(".nav-select-month select", "May");
