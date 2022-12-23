8059e0d6eb76d3fbbac2f54c6ffec8e99552ce98
Falk <falk.neumann@adfinis.com>
2022-12-23 11:06:21 +0100

    chore(lint): configure commitlint and ci linting
    We want to trigger errors in commitlint and warnings in the CI for ember
    deprecations, which do work for now (3.28 LTS) but will not work in the future
    (>v4), to allow for gradual code adaption when touching the code.

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,11 +3,15 @@
 module.exports = {
   extends: ["@adfinis-sygroup/eslint-config/ember-app"],
   rules: {
+    "ember/no-actions-hash": "warn",
+    "ember/no-component-lifecycle-hooks": "warn",
     "ember/no-mixins": "warn",
     "ember/no-new-mixins": "warn",
     "ember/no-classic-classes": "warn",
+    "ember/no-classic-components": "warn",
     "ember/no-get": "warn",
     "ember/no-observers": "warn",
     "qunit/no-assert-equal": "warn",
+    "ember/require-tagless-components": "warn",
   },
 };
---
+++ .template-lintrc-ci.js
@@ -0,0 +1,19 @@
+"use strict";
+
+module.exports = {
+  extends: "recommended",
+  rules: {
+    // following rules are for temporary use only, delete when ember 4.0 ready
+    "no-action": "warn",
+    "no-curly-component-invocation": "warn",
+    "no-duplicate-id": "warn",
+    "no-link-to-positional-params": "warn",
+    "no-implicit-this": "warn",
+    "no-passed-in-event-handlers": "warn",
+    "no-positional-data-test-selectors": "warn",
+    "no-unknown-arguments-for-builtin-components": "warn",
+    "no-yield-only": "warn",
+    "require-input-label": "warn",
+    "require-has-block-helper": "warn",
+  },
+};
