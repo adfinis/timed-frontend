26aaaa6001ec916fe3ecb8ac886060dbf2486812
Falk <falk.neumann@adfinis.com>
2023-01-25 16:44:07 +0100

    fix(statistics): statistic-list-bar display goal and remainig effort only if defined

--- app/models/task-statistic.js
+++ app/models/task-statistic.js
@@ -1,11 +1,9 @@
 import Model, { attr, belongsTo } from "@ember-data/model";
-import moment from "moment";
 
 export default class TaskStatistics extends Model {
   @attr name;
   @attr("django-duration") duration;
-  @attr("django-duration", { defaultValue: () => moment.duration() })
-  estimatedTime;
+  @attr("django-duration") estimatedTime;
   @attr("django-duration") mostRecentRemainingEffort;
   @belongsTo("project") project;
 }
