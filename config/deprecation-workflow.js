70c7f736de5c5701124d9bef20300c76341a97cf
Mitan Omar <mitan@gmx.ch>
2023-03-28 13:49:18 +0200

    fix: checking the others deprecation, and testing

--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -2,19 +2,21 @@
 window.deprecationWorkflow = window.deprecationWorkflow || {};
 window.deprecationWorkflow.config = {
   workflow: [
-    { handler: "silence", matchId: "ember-global" },
-    { handler: "silence", matchId: "ember.component.reopen" },
-    { handler: "silence", matchId: "ember-metal.get-with-default" },
+    { handler: "throw", matchId: "ember-global" },
+    { handler: "silence", matchId: "ember.component.reopen" }, // not finish yet
+    { handler: "throw", matchId: "ember-metal.get-with-default" },
     { handler: "silence", matchId: "implicit-injections" },
     {
-      handler: "silence",
+      // Fixed
+      handler: "throw",
       matchId: "ember-views.curly-components.jquery-element",
     },
     {
+      // the issue exists in ember scrollable package
       handler: "silence",
       matchId: "deprecated-run-loop-and-computed-dot-access",
     },
-    { handler: "silence", matchId: "ember-utils.try-invoke" },
+    // { handler: "throw", matchId: "ember-utils.try-invoke" }, something in ember core
     { handler: "silence", matchId: "this-property-fallback" },
     { handler: "throw", matchId: "autotracking.mutation-after-consumption" },
     { handler: "throw", matchId: "manager-capabilities.modifiers-3-13" },
