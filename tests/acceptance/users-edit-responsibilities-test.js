7bc24c63bb5422b35a15b551f711ebe583b1b506
Mitan Omar <mitan@gmx.ch>
2023-04-03 17:12:36 +0200

    fix: writing tests for new change

--- tests/acceptance/users-edit-responsibilities-test.js
+++ tests/acceptance/users-edit-responsibilities-test.js
@@ -1,4 +1,4 @@
-import { visit } from "@ember/test-helpers";
+import { visit, currentURL, click } from "@ember/test-helpers";
 import { setupMirage } from "ember-cli-mirage/test-support";
 import { setupApplicationTest } from "ember-qunit";
 import { authenticateSession } from "ember-simple-auth/test-support";
@@ -9,7 +9,16 @@ module("Acceptance | users edit responsibilities", function (hooks) {
   setupMirage(hooks);
 
   hooks.beforeEach(async function () {
-    this.user = this.server.create("user");
+    this.user = this.server.create("user", {
+      isSuperuser: true,
+      firstName: "Admin FirstName",
+      lastName: "Admin LastName",
+    });
+    this.secondUser = this.server.create("user", {
+      supervisorIds: [this.user.id],
+      firstName: "FirstName",
+      lastName: "LastName",
+    });
 
     // eslint-disable-next-line camelcase
     await authenticateSession({ user_id: this.user.id });
@@ -19,5 +28,11 @@ module("Acceptance | users edit responsibilities", function (hooks) {
     await visit(`/users/1/responsibilities`);
 
     assert.dom(".card").exists({ count: 2 });
+  });
+
+  test("Super user can visit the users profile", async function (assert) {
+    await visit(`/users/1/responsibilities`);
+    await click("tbody tr:nth-child(2)");
+    assert.strictEqual(currentURL(), `/users/${this.secondUser.id}`);
   });
 });
