e6d10d38ef7d3bcabe25066a4b88b05d2f07b575
Mitan Omar <mitan@gmx.ch>
2023-04-13 15:08:57 +0200

    fix: query param change trigger a full transition

--- app/users/index/route.js
+++ app/users/index/route.js
@@ -1,3 +1,22 @@
 import Route from "@ember/routing/route";
 
-export default Route;
+export default class UsersIndexRoute extends Route {
+  queryParams = {
+    search: {
+      refreshModel: true,
+      replace: true,
+    },
+    supervisor: {
+      refreshModel: true,
+      replace: true,
+    },
+    active: {
+      refreshModel: true,
+      replace: true,
+    },
+    ordering: {
+      refreshModel: true,
+      replace: true,
+    },
+  };
+}
