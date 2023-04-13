f5a16509c46cccb774456caa7e58e821ab6a57ec
Mitan Omar <mitan@gmx.ch>
2023-04-13 16:21:06 +0200

    refactor: admin users list

--- app/users/index/controller.js
+++ app/users/index/controller.js
@@ -3,8 +3,6 @@ import { action, set } from "@ember/object";
 import { inject as service } from "@ember/service";
 import { tracked } from "@glimmer/tracking";
 import { restartableTask, timeout, hash } from "ember-concurrency";
-import { task as trackedTask } from "ember-resources/util/ember-concurrency";
-import moment from "moment";
 
 export default class UsersIndexController extends Controller {
   queryParams = ["search", "supervisor", "active", "ordering"];
@@ -21,32 +19,12 @@ export default class UsersIndexController extends Controller {
     this.prefetchData.perform();
   }
 
-  _fetchData = trackedTask(this, this.data, () => [
-    this.supervisor,
-    this.search,
-    this.ordering,
-    this.active,
-  ]);
-
   get selectedSupervisor() {
     return this.supervisor && this.store.peekRecord("user", this.supervisor);
   }
 
   get currentUser() {
     return this.session.data.user;
-  }
-
-  get fetchData() {
-    return this._fetchData ?? {};
-  }
-
-  get allQueryParams() {
-    return {
-      supervisor: this.supervisor,
-      search: this.search,
-      active: this.active,
-      ordering: this.ordering,
-    };
   }
 
   resetQueryParams() {
@@ -72,24 +50,6 @@ export default class UsersIndexController extends Controller {
 
     return yield hash({
       supervisor: supervisorId && this.store.findRecord("user", supervisorId),
-    });
-  }
-
-  @restartableTask
-  *data() {
-    yield Promise.resolve();
-    const date = moment().format("YYYY-MM-DD");
-
-    yield this.store.query("employment", { date });
-    yield this.store.query("worktime-balance", { date });
-
-    return yield this.store.query("user", {
-      ...this.allQueryParams,
-      ...(this.currentUser.isSuperuser
-        ? {}
-        : {
-            supervisor: this.currentUser.id,
-          }),
     });
   }
 
--- app/users/index/route.js
+++ app/users/index/route.js
@@ -1,6 +1,11 @@
 import Route from "@ember/routing/route";
+import { inject as service } from "@ember/service";
+import moment from "moment";
 
 export default class UsersIndexRoute extends Route {
+  @service session;
+  @service store;
+
   queryParams = {
     search: {
       refreshModel: true,
@@ -19,4 +24,25 @@ export default class UsersIndexRoute extends Route {
       replace: true,
     },
   };
+
+  async model(params) {
+    try {
+      const date = moment().format("YYYY-MM-DD");
+
+      await this.store.query("employment", { date });
+      await this.store.query("worktime-balance", { date });
+
+      const data = await this.store.query("user", {
+        ...params,
+        ...(this.session.data.user.isSuperuser
+          ? {}
+          : {
+              supervisor: this.session.data.user.id,
+            }),
+      });
+      return { value: data };
+    } catch (error) {
+      return { isError: true };
+    }
+  }
 }
