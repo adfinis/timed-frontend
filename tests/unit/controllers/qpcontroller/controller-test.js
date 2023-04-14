18693e082ff648a27b5adef912846f438de4514a
Falk <falk.neumann@adfinis.com>
2023-04-14 11:05:22 +0200

    test(qpcontroller): add basic coverage

--- tests/unit/controllers/qpcontroller/controller-test.js
+++ tests/unit/controllers/qpcontroller/controller-test.js
@@ -4,9 +4,25 @@ import { module, test } from "qunit";
 module("Unit | Controller | controllers/qpcontroller", function (hooks) {
   setupTest(hooks);
 
-  // TODO: Replace this with your real tests.
-  test("it exists", function (assert) {
-    const controller = this.owner.lookup("controller:controllers/qpcontroller");
-    assert.ok(controller);
+  test("get all query params", function (assert) {
+    const controller = this.owner.lookup("controller:qpcontroller");
+    controller.queryParams = ["qp1", "qp2"];
+    controller.qp1 = "baz";
+    controller.qp2 = "foo";
+
+    assert.strictEqual(controller.allQueryParams.qp1, "baz");
+    assert.strictEqual(controller.allQueryParams.qp2, "foo");
+  });
+
+  test("reset query params", function (assert) {
+    const controller = this.owner.lookup("controller:qpcontroller");
+    controller.queryParams = ["qp1", "qp2"];
+    controller.qp1 = "baz";
+    controller.qp2 = "foo";
+
+    controller.resetQueryParams();
+
+    assert.strictEqual(controller.allQueryParams.qp1, undefined);
+    assert.strictEqual(controller.allQueryParams.qp2, undefined);
   });
 });
