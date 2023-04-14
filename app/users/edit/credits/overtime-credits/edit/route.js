aeec8c39836be504e1ec16646c168c1833c5455c
Mitan Omar <mitan@gmx.ch>
2023-04-14 18:59:50 +0200

    fix: use native js classes in routes

--- app/users/edit/credits/overtime-credits/edit/route.js
+++ app/users/edit/credits/overtime-credits/edit/route.js
@@ -1,12 +1,12 @@
 import Route from "@ember/routing/route";
 
-export default Route.extend({
-  model: ({ overtime_credit_id: id }) => id,
+export default class NewUserOvertimeCreditRoute extends Route {
+  model = ({ overtime_credit_id: id }) => id;
 
   setupController(controller, ...args) {
-    this._super(controller, ...args);
+    super.setupController(controller, ...args);
 
     controller.set("user", this.modelFor("users.edit"));
     controller.credit.perform();
-  },
-});
+  }
+}
--- app/users/edit/credits/overtime-credits/new/route.js
+++ app/users/edit/credits/overtime-credits/new/route.js
@@ -2,17 +2,17 @@ import Route from "@ember/routing/route";
 
 const EDIT_PATH = "users.edit.credits.overtime-credits.edit";
 
-export default Route.extend({
-  controllerName: EDIT_PATH,
+export default class NewUserOvertimeCreditRoute extends Route {
+  controllerName = EDIT_PATH;
 
-  templateName: EDIT_PATH,
+  templateName = EDIT_PATH;
 
-  model: () => null,
+  model = () => null;
 
   setupController(controller, ...args) {
-    this._super(controller, ...args);
+    super.setupController(controller, ...args);
 
     controller.set("user", this.modelFor("users.edit"));
     controller.credit.perform();
-  },
-});
+  }
+}
