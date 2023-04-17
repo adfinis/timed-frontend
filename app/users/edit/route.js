068dedc7c130265b88d35e60a8ae52f804586656
Mitan Omar <mitan@gmx.ch>
2023-04-17 13:53:50 +0200

    fix: run data fetching on setup controller hook in users edit route

--- app/users/edit/route.js
+++ app/users/edit/route.js
@@ -4,4 +4,9 @@ export default class EditUserRoute extends Route {
   model({ user_id: id }) {
     return this.store.findRecord("user", id, { include: "supervisors" });
   }
+
+  setupController(controller, model, ...args) {
+    super.setupController(controller, model, ...args);
+    controller.data.perform(model.id);
+  }
 }
