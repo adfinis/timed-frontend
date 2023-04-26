699906ea746bbc603935a2465dac2fd8ea5b17e7
Mitan Omar <mitan@gmx.ch>
2023-04-26 04:06:49 +0200

    feat: writing tests for chart rendering

--- tests/integration/components/statistic-list/bar/component-test.js
+++ tests/integration/components/statistic-list/bar/component-test.js
@@ -18,4 +18,30 @@ module("Integration | Component | statistic list/bar", function (hooks) {
       "0.5"
     );
   });
+
+  test("The Chart color is RED when spent effort is over the budget", async function (assert) {
+    await render(
+      hbs`<StatisticList::Bar @value={{0.5}} @goal={{0.4}} @remaining={{0}} />`
+    );
+
+    const element = this.element.querySelector(".statistic-list-bar");
+
+    assert.ok(element);
+
+    assert.true(element.classList.contains("strong-danger"));
+    assert.false(element.classList.contains("strong-success"));
+  });
+
+  test("The Chart color is GREEN when spent effort is in the budget", async function (assert) {
+    await render(
+      hbs`<StatisticList::Bar @value={{0.5}} @goal={{0.6}} @remaining={{0}} />`
+    );
+
+    const element = this.element.querySelector(".statistic-list-bar");
+
+    assert.ok(element);
+
+    assert.false(element.classList.contains("strong-danger"));
+    assert.true(element.classList.contains("strong-success"));
+  });
 });
