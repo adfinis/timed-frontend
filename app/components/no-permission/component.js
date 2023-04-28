69d613efb17197e5d8d73cd64fd3a2c830bf2d56
Mitan Omar <mitan@gmx.ch>
2023-04-28 15:38:01 +0200

    refactor(no-permission): to glimmer syntax

--- app/components/no-permission/component.js
+++
@@ -1,7 +0,0 @@
-import Component from "@ember/component";
-import { classNames } from "@ember-decorators/component";
-import classic from "ember-classic-decorator";
-
-@classic
-@classNames("empty")
-export default class NoPermission extends Component {}
