3fc56488b442972d3442e28a205749b846d289ad
Mitan Omar <mitan@gmx.ch>
2023-05-12 11:45:51 +0200

    fix(tests): parameter compatibility of reports abilities

--- tests/unit/abilities/report-test.js
+++ tests/unit/abilities/report-test.js
@@ -1,3 +1,4 @@
+import EmberObject from "@ember/object";
 import { setupTest } from "ember-qunit";
 import { module, test } from "qunit";
 
@@ -6,52 +7,57 @@ module("Unit | Ability | report", function (hooks) {
 
   test("can edit when user is superuser", function (assert) {
     const ability = this.owner.lookup("ability:report");
-    ability.set("user", { isSuperuser: true });
+    ability.set("user", EmberObject.create({ isSuperuser: true }));
 
     assert.true(ability.get("canEdit"));
   });
 
   test("can edit when user is superuser and report is verified", function (assert) {
     const ability = this.owner.lookup("ability:report");
-    ability.set("user", { isSuperuser: true });
-    ability.set("model", { verifiedBy: { id: 1 } });
+    ability.set("user", EmberObject.create({ isSuperuser: true }));
+    ability.set("model", { verifiedBy: EmberObject.create({ id: 1 }) });
 
     assert.true(ability.get("canEdit"));
   });
 
   test("can edit when user owns report", function (assert) {
     const ability = this.owner.lookup("ability:report");
-    ability.set("user", { id: 1 });
-    ability.set("model", { user: { id: 1 } });
+    ability.set("user", EmberObject.create({ id: 1 }));
+    ability.set("model", { user: EmberObject.create({ id: 1 }) });
 
     assert.true(ability.get("canEdit"));
   });
 
   test("can edit when user is supervisor of owner", function (assert) {
     const ability = this.owner.lookup("ability:report");
-    ability.set("user", { id: 1 });
-    ability.set("model", { user: { supervisors: [{ id: 1 }] } });
+    ability.set("user", EmberObject.create({ id: 1 }));
+    ability.set("model", {
+      user: EmberObject.create({ supervisors: [{ id: 1 }] }),
+    });
 
     assert.true(ability.get("canEdit"));
   });
 
   test("can edit when user reviewer of project", function (assert) {
     const ability = this.owner.lookup("ability:report");
-    const user = { id: 1 };
+    const user = EmberObject.create({ id: 1 });
     const projectAssignee = [{ user }];
     ability.set("user", user);
-    ability.set("model", {
-      projectAssignees: projectAssignee,
-    });
+    ability.set(
+      "model",
+      EmberObject.create({
+        projectAssignees: projectAssignee,
+      })
+    );
 
     assert.true(ability.get("canEdit"));
   });
 
   test("can not edit when not allowed", function (assert) {
     const ability = this.owner.lookup("ability:report");
-    ability.set("user", { id: 1, isSuperuser: false });
+    ability.set("user", EmberObject.create({ id: 1, isSuperuser: false }));
     ability.set("model", {
-      user: { id: 2, supervisors: [{ id: 2 }] },
+      user: EmberObject.create({ id: 2, supervisors: [{ id: 2 }] }),
       task: { project: { reviewers: [{ id: 2 }] } },
       projectAssignees: [{ id: 2 }],
     });
@@ -61,11 +67,11 @@ module("Unit | Ability | report", function (hooks) {
 
   test("can not edit when report is verified and billed", function (assert) {
     const ability = this.owner.lookup("ability:report");
-    ability.set("user", { id: 1, isSuperuser: false });
+    ability.set("user", EmberObject.create({ id: 1, isSuperuser: false }));
     ability.set("model", {
-      user: { id: 1, supervisors: [{ id: 1 }] },
+      user: EmberObject.create({ id: 1, supervisors: [{ id: 1 }] }),
       projectAssignees: [{ id: 1 }],
-      verifiedBy: { id: 1 },
+      verifiedBy: EmberObject.create({ id: 1 }),
       billed: true,
     });
 
