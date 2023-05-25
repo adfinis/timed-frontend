0be39d63beb3519fc6db0a6c03381370d9a1b40d
Mitan Omar <mitan@gmx.ch>
2023-05-25 20:08:17 +0200

    refactor(weekly-overview-benchmark): to glimmer syntax

--- app/components/weekly-overview-benchmark/component.js
+++ app/components/weekly-overview-benchmark/component.js
@@ -1,23 +1,6 @@
-/**
- * @module timed
- * @submodule timed-components
- * @public
- */
-import Component from "@ember/component";
-import { htmlSafe } from "@ember/string";
-import { classNameBindings } from "@ember-decorators/component";
+import Component from "@glimmer/component";
 import { tracked } from "@glimmer/tracking";
-import classic from "ember-classic-decorator";
 
-/**
- * Component to show a benchmark (reached worktime) in the weekly overview
- *
- * @class WeeklyOverviewBenchmarkComponent
- * @extends Ember.Component
- * @public
- */
-@classic
-@classNameBindings("expected")
 export default class WeeklyOverviewBenchmark extends Component {
   /**
    * Maximum worktime
@@ -30,36 +13,12 @@ export default class WeeklyOverviewBenchmark extends Component {
   @tracked max = 20;
 
   /**
-   * Hours of the benchmark
-   *
-   * @property {Number} hours
-   * @public
-   */
-  @tracked hours = 0;
-
-  /**
-   * Whether it is the expected worktime
-   *
-   * @property {Boolean} expected
-   * @public
-   */
-  expected = false;
-
-  /**
-   * Whether to show the hour label
-   *
-   * @property {Boolean} showLabel
-   * @public
-   */
-  showLabel = false;
-
-  /**
    * The offset to the bottom
    *
    * @property {String} style
    * @public
    */
   get style() {
-    return htmlSafe(`bottom: calc(100% / ${this.max} * ${this.hours})`);
+    return { bottom: `calc(100% / ${this.max} * ${this.args.hours})` };
   }
 }
