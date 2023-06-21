0610877af9da564dc4f09ecd477110ae47e830a4
Mitan Omar <mitan@gmx.ch>
2023-06-21 18:13:08 +0200

    refactor(reports-ability): use getter instead of local state

--- tests/unit/abilities/report-test.js
+++ tests/unit/abilities/report-test.js
@@ -1,20 +1,24 @@
 import EmberObject from "@ember/object";
 import { setupTest } from "ember-qunit";
 import { module, test } from "qunit";
+import setupSession from "timed/tests/helpers/session-mock";
 
 module("Unit | Ability | report", function (hooks) {
   setupTest(hooks);
+  setupSession(hooks);
 
   test("can edit when user is superuser", function (assert) {
     const ability = this.owner.lookup("ability:report");
-    ability.set("user", EmberObject.create({ isSuperuser: true }));
+    const session = this.owner.lookup("service:session");
+    session.data = { user: EmberObject.create({ isSuperuser: true }) };
 
     assert.true(ability.get("canEdit"));
   });
 
   test("can edit when user is superuser and report is verified", function (assert) {
     const ability = this.owner.lookup("ability:report");
-    ability.set("user", EmberObject.create({ isSuperuser: true }));
+    const session = this.owner.lookup("service:session");
+    session.data = { user: EmberObject.create({ isSuperuser: true }) };
     ability.set("model", { verifiedBy: EmberObject.create({ id: 1 }) });
 
     assert.true(ability.get("canEdit"));
@@ -22,7 +26,8 @@ module("Unit | Ability | report", function (hooks) {
 
   test("can edit when user owns report", function (assert) {
     const ability = this.owner.lookup("ability:report");
-    ability.set("user", EmberObject.create({ id: 1 }));
+    const session = this.owner.lookup("service:session");
+    session.data = { user: EmberObject.create({ id: 1 }) };
     ability.set("model", { user: EmberObject.create({ id: 1 }) });
 
     assert.true(ability.get("canEdit"));
@@ -30,7 +35,8 @@ module("Unit | Ability | report", function (hooks) {
 
   test("can edit when user is supervisor of owner", function (assert) {
     const ability = this.owner.lookup("ability:report");
-    ability.set("user", EmberObject.create({ id: 1 }));
+    const session = this.owner.lookup("service:session");
+    session.data = { user: EmberObject.create({ id: 1 }) };
     ability.set("model", {
       user: EmberObject.create({ supervisors: [{ id: 1 }] }),
     });
@@ -42,7 +48,8 @@ module("Unit | Ability | report", function (hooks) {
     const ability = this.owner.lookup("ability:report");
     const user = EmberObject.create({ id: 1 });
     const projectAssignee = [{ user }];
-    ability.set("user", user);
+    const session = this.owner.lookup("service:session");
+    session.data = { user };
     ability.set(
       "model",
       EmberObject.create({
@@ -55,7 +62,8 @@ module("Unit | Ability | report", function (hooks) {
 
   test("can not edit when not allowed", function (assert) {
     const ability = this.owner.lookup("ability:report");
-    ability.set("user", EmberObject.create({ id: 1, isSuperuser: false }));
+    const session = this.owner.lookup("service:session");
+    session.data = { user: EmberObject.create({ id: 1, isSuperuser: false }) };
     ability.set("model", {
       user: EmberObject.create({ id: 2, supervisors: [{ id: 2 }] }),
       task: { project: { reviewers: [{ id: 2 }] } },
@@ -67,7 +75,8 @@ module("Unit | Ability | report", function (hooks) {
 
   test("can not edit when report is verified and billed", function (assert) {
     const ability = this.owner.lookup("ability:report");
-    ability.set("user", EmberObject.create({ id: 1, isSuperuser: false }));
+    const session = this.owner.lookup("service:session");
+    session.data = { user: EmberObject.create({ id: 1, isSuperuser: false }) };
     ability.set("model", {
       user: EmberObject.create({ id: 1, supervisors: [{ id: 1 }] }),
       projectAssignees: [{ id: 1 }],
