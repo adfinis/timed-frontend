22fb1251366d9558aed839fc34d8829e0ba58f98
Mitan Omar <mitan@gmx.ch>
2023-08-02 13:50:12 +0200

    fix: deprecation workflows (#1024)
    * fix: remove the fixed deprecations
    
    * refactor(ember-can): usage to fix the deprecation
    
    * refactor(user-model): to fix the deprecation
    
    * refactor(meber-cli-mirage): to fix the deprecation

--- app/users/edit/credits/index/controller.js
+++ app/users/edit/credits/index/controller.js
@@ -2,7 +2,6 @@ import Controller, { inject as controller } from "@ember/controller";
 import { action } from "@ember/object";
 import { inject as service } from "@ember/service";
 import { tracked } from "@glimmer/tracking";
-import { ability } from "ember-can/computed";
 import { dropTask, restartableTask } from "ember-concurrency";
 import moment from "moment";
 
@@ -37,14 +36,11 @@ export default class UsersEditCredits extends Controller {
     return [...new Array(to + 1 - from).keys()].map((i) => `${from + i}`);
   }
 
-  @ability("overtime-credit") overtimeCreditAbility;
-  @ability("absence-credit") absenceCreditAbility;
-
   get allowTransfer() {
     return (
       parseInt(this.year) === moment().year() - 1 &&
-      this.overtimeCreditAbility.canCreate &&
-      this.absenceCreditAbility.canCreate
+      this.can.can("create overtime-credit") &&
+      this.can.can("create absence-credit")
     );
   }
 
