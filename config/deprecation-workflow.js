48c29a82dc8e3d801bdc612fcad24861cac9b676
Falk <falk.neumann@adfinis.com>
2023-09-27 15:35:45 +0200

    fix: replace ember-scrollable with simplebar

--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -3,10 +3,5 @@ window.deprecationWorkflow = window.deprecationWorkflow || {};
 self.deprecationWorkflow.config = {
   workflow: [
     { handler: "silence", matchId: "ensure-safe-component.string" }, // optimized-power-select
-    {
-      handler: "silence",
-      matchId: "deprecated-run-loop-and-computed-dot-access", // ember scrollable
-    },
-    { handler: "silence", matchId: "this-property-fallback" }, // ember scrollable
   ],
 };
