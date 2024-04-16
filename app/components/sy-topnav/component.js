668067fe2035075195857f2dfc64eb0e5c4af337
Mitan Omar <mitan@gmx.ch>
2024-04-16 17:42:30 +0200

    fix: the user info in navigation return empty string after refreshing the token

--- app/components/sy-topnav/component.js
+++ app/components/sy-topnav/component.js
@@ -12,4 +12,21 @@ export default class SyTopnav extends Component {
   get navMobile() {
     return this.media.isMo || this.media.isXs || this.media.isSm;
   }
+
+  /**
+   * TODO:
+   * i don't know if it's an issue in `ember-simple-auth` or `ember-simple-auth-oidc`
+   * or it's maybe wrong configration from Timed frontend side,
+   * BUT after refreshing the token, the session service is not encapsulating the reslts from keycloak
+   * inside ember data model, so i add this small.
+   */
+  get fullName() {
+    const fullName = this.session.data.user?.fullName;
+    if (fullName) return fullName;
+
+    if (!this.session.data.user?.attributes) return "";
+
+    const userAttributes = this.session.data.user.attributes;
+    return `${userAttributes["first-name"]} ${userAttributes["last-name"]}`;
+  }
 }
